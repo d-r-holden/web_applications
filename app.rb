35a2886cd2e9d67d9fe705fd014ca53547b080ad
d-r-holden <117095219+d-r-holden@users.noreply.github.com>
2023-01-19 10:19:42 +0000

    Add files via upload

--- app.rb
+++ app.rb
@@ -10,14 +10,33 @@ class Application < Sinatra::Base
   
   get '/albums' do
     repo = AlbumRepository.new
-    albums = repo.all
+    @albums = repo.all
+    return erb(:albums)
 
-    response = albums.map do |album|
-      album.title
+    #response = albums.map do |album|
+    #  album.title
     
-    end.join(', ')
+    #end.join(', ')
   
-    return response
+    #return response
+  end
+
+  get '/albums/:id' do
+    repo = AlbumRepository.new
+    artist_repo = ArtistRepository.new
+
+    @album = repo.find(params[:id])
+    @artist = artist_repo.find(@album.artist_id)
+
+    return erb(:album)
+  end
+
+  get '/artists/:id' do
+    repo = ArtistRepository.new
+
+    @artist = repo.find(params[:id])
+
+    return erb(:artist)
   end
   
   
@@ -35,12 +54,12 @@ class Application < Sinatra::Base
 
   get '/artists' do
     repo = ArtistRepository.new
-    artists = repo.all
+    @artists = repo.all
 
-    response = artists.map do |artist|
-      artist.name
-    end.join(', ')
-    return response
+    #response = artists.map do |artist|
+    #  artist.name
+    #end.join(', ')
+    return erb(:artists)
   end
   
   post '/artists' do
